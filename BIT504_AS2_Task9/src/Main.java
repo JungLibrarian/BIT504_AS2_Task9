688ce86fb84556f1e540088023bb6c97e7243cbe
Jung Cho <junglibrarian@gmail.com>
2026-02-28 13:07:02 +1300

    Comments are added to the branch

--- BIT504_AS2_Task9/src/Main.java
+++ BIT504_AS2_Task9/src/Main.java
@@ -6,12 +6,15 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
         
+		// ask first name
         System.out.println("Enger your first name: ");
         String fName = scanner.next();
         
+        // ask last name        
         System.out.println("Enger your last name: ");
         String lName = scanner.next();
         
+        // output first name and last name
         System.out.println("Hello, " + fName + " " + lName);
         
         scanner.close();
